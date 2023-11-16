43c170e4912b5b9fbf7ece09a3f4da0eefc2e2eb
Arkadiy <goryunov.arkadiy@gmail.com>
2023-11-16 13:48:16 +0700

    Add validation for phone number and email, update date validation

--- src/app/validator.py
+++ src/app/validator.py
@@ -1,4 +1,6 @@
+import calendar
 from datetime import datetime
+import re
 
 
 class Validator:
@@ -8,23 +10,26 @@ class Validator:
         date_pattern_2 = "%Y-%m-%d"
 
         try:
-            datetime.strptime(input_string, date_pattern_1)
-            return True
+            dt = datetime.strptime(input_string, date_pattern_1)
         except ValueError:
-            pass
+            try:
+                dt = datetime.strptime(input_string, date_pattern_2)
+            except ValueError:
+                return False
 
-        try:
-            datetime.strptime(input_string, date_pattern_2)
-            return True
-        except ValueError:
-            pass
-
-        return False
+        _, last_day = calendar.monthrange(dt.year, dt.month)
+        if dt.day <= last_day:
+            return False
+        return True
 
     @staticmethod
     def is_input_string_phone_number(input_string: str) -> bool:
-        pass
+        phone_pattern = re.compile(r'^\+7 \d{3} \d{3} \d{2} \d{2}$')
+        match = re.match(phone_pattern, input_string)
+        return bool(match)
 
     @staticmethod
     def is_input_string_email(input_string: str) -> bool:
-        pass
+        email_pattern = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+        match = re.match(email_pattern, input_string)
+        return bool(match)
