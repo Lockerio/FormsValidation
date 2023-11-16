22528ef9ca98e9806686bba28e75cd2e72953c7f
Arkadiy <goryunov.arkadiy@gmail.com>
2023-11-16 22:21:04 +0700

    Add get_form route

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, Response
+from flask import Blueprint, jsonify, Response, request
 
 from app.database.database import db
 
@@ -12,3 +12,15 @@ def get_all_forms() -> Response:
         return jsonify({'collections': collections})
     else:
         return jsonify({'response': 'no such collections'})
+
+
+@routes_blueprint.route('/get_form', methods=['POST'])
+def get_form() -> Response:
+    query_params = request.args
+    collection = db["forms"]
+
+    filter_query = {field: query_params[field] for field in query_params}
+
+    result = list(collection.find(filter_query))
+    return jsonify(result)
+
